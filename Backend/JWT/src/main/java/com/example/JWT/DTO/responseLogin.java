f13e0e546555ad4cf047425cb73e6024e64e6a9a
martin <martinstibennarvaez2006@gmail.com>
2025-06-12 11:12:39 -0500

    JWT 1.4

--- Backend/JWT/src/main/java/com/example/JWT/DTO/responseLogin.java
+++ Backend/JWT/src/main/java/com/example/JWT/DTO/responseLogin.java
@@ -1,42 +1,12 @@
 package com.example.JWT.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class responseLogin {
-    
     private String token;
-    private String username;
-    private String email;
-    
-    public responseLogin() {
-    }
-    
-    public responseLogin(String token, String username, String email) {
-        this.token = token;
-        this.username = username;
-        this.email = email;
-    }
-    
-    // Getters y setters
-    public String getToken() {
-        return token;
-    }
-    
-    public void setToken(String token) {
-        this.token = token;
-    }
-    
-    public String getUsername() {
-        return username;
-    }
-    
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    
-    public String getEmail() {
-        return email;
-    }
-    
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
